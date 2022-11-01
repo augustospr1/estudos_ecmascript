ab448e8b857dfdb12338c392631202e3c648b530
Augusto Sérgio <86673464+augustospr1@users.noreply.github.com>
2022-11-01 20:53:59 -0300

    learn methods

--- js-antes-do-react/main.js
+++ js-antes-do-react/main.js
@@ -92,4 +92,54 @@
 
 // document.body.innerText = user.address?.zip?.code ?? 'Não informado'; // Código simplificando
 
-document.body.innerText = user.address?.showFullAddress();
+// document.body.innerText = user.address?.showFullAddress();
+
+// ################ Métodos de array map() filter() forEach() every() some() find() reduce()  ###################
+
+const array = [1, 2, 3, 4, 5];
+
+// for (const i of array) {
+//   document.body.innerText += i;  // O for foi utilizado para percorrer cada elemento do array
+// }
+
+// array.forEach(item => {
+//   document.body.innerText += item; // O método forEach foi utilizado também para percorrer cada um dos elementos e a função foi utilizado para printar cada um deles.
+// })
+
+// const novoArray = array.map(item => {
+//   return item * 2;                          // Esse método map() é utilizado para percorrer SOMENTE todo o array e retornar ele modificado por exemplo, multiplicando ou dividindo todos os elementos contidos dentro dele.
+// })
+
+// const novoArray = array.map(item => {
+//   if (item % 2 === 0) {
+//     return item * 10;
+//   }                         // Seguindo um exemplo, foi feita uma função que pega SOMENTE os items pares do array e multiplica por 10, e retorna os items posteriormente.
+//   return item;
+// })
+
+// const novoArray = array.filter(item => item % 2 === 0).map(item => item * 10);  // Nesse caso foi concatenado dois métodos, que foi o filter() justamente para filtrar somente os elementos pares do array e após isso foi utilizado o método map() para multiplicar os elementos filtrados por 10.
+
+// const todosItensSaoNumeros = array.every(item => typeof item === 'number') // O método every() é utilizado para verificar se todos os elementos de um array pertence a um grupo específico por exemplo, string ou números. E ao verificar ele retorna um valor booleano que é true ou false.
+
+// const peloMenosUmItemEUmNumero = array.some(item => typeof item === 'string') // O método some() verifica se dentro do array existe nem que seja apenas UM ITEM que ele procura de determinado grupo por exemplo, número ou string e se caso exista ele retorna um valor booleano true ou false.
+
+// const par = array.find(item => item % 2 === 0); // Esse método verifica dentro do array o primeiro elemento que corresponde ao verificamento que ele faz, por exemplo, o primeiro elemento par que ele encontrar.
+
+// const par = array.findIndex(item => item % 2 === 0); // Esse método verifica qual é o índice do elemento que foi filtrado. Nesse exemplo ele pegou o primeiro número par e verificou qual era o índice dele e imprime na tela.
+
+// const soma = array.reduce((acc, item) => {
+//   document.body.innerText += acc + ',' + item + ' --- ' 
+                                                            // Com o método reduce() você pode reduzir seu array para várias coisas, como exemplo, um outro array, ou em um objeto.
+//   return acc + item
+// }, 0)
+
+// document.body.innerText = JSON.stringify(soma)
+
+// ###################### Template Literals #######################
+
+// const name = 'Augusto'
+// const message = `Bem vindo, ${name ? name : 'visitante'}`; // É utilizado quando vc quer incluir uma variável dentro de uma mensagem, colocando algumas condições para aparecer.
+
+// document.body.innerText = JSON.stringify(message)
+
+// ###################### Promises ########################
